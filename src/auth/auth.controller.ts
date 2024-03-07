659f7bc58dcbaf79ac10e6ea6251f82b241ce254
hongvanpc10 <hongvanpc10@gmail.com>
2024-03-07 11:07:50 +0700

    abc

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -26,7 +26,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'none',
-      maxAge: 30 * 24 * 60 * 60 * 1000
+      maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
     return { accessToken, user };
